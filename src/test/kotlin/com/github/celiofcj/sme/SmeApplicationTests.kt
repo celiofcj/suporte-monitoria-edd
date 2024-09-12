ea6f52248cb59933f1125d590017eed3dd8168bf
Celio Camara <celiofcjunior_cg@hotmail.com>
2024-09-11 23:36:39 -0300

    first commit

---
+++ src/test/kotlin/com/github/celiofcj/sme/SmeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.celiofcj.sme
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SmeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
